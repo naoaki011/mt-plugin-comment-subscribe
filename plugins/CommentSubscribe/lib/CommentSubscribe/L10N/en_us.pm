aec05398da4e262f8f01187f9160829410acfe3f
naoaki011 <naoaki@orion.ocn.ne.jp>
2011-02-20 00:44:01 +0900

    Japanese L10N Support.

---
+++ plugins/CommentSubscribe/lib/CommentSubscribe/L10N/en_us.pm
@@ -0,0 +1,9 @@
+package CommentSubscribe::L10N::en_us;
+
+use strict;
+
+use base 'CommentSubscribe::L10N';
+use vars qw( %Lexicon );
+%Lexicon = ();
+
+1;
---
+++ plugins/CommentSubscribe/lib/CommentSubscribe/L10N/ja.pm
@@ -0,0 +1,20 @@
+package CommentSubscribe::L10N::ja;
+
+use strict;
+use base 'CommentSubscribe::L10N::en_us';
+use vars qw( %Lexicon );
+
+%Lexicon = (
+
+    'Allows viewers to subscribe to recieve emails every time a comment is posted for a given entry.'
+      => 'エントリーにコメントが投稿されたときに、そのコメントをメールで受信できるようにします。',
+    "([_1]) [_2] posted a new comment on '[_3]'" => "([_1]) [_2] さんからブログ記事「[_3]」にコメントがありました。",
+    "There's a new comment on '[_1]'" => "ブログ記事「[_1]」に新しいコメントがありました。",
+    'If you no longer wish to receive notifications, please click here' => 'コメントの通知メールの送信を止めたい場合は、次のアドレスをクリックしてください。',
+    'If you no longer wish to receive notifications of new comments, please click or cut and paste the following URL into a web browser'
+      => '新規コメントの通知メール送信を止めたい場合は、次のURLをクリックするかブラウザーに貼り付けてください。',
+    'You have successfully unsubscribed from future comment notifications from [_1].' => '[_1]に対する、以後のコメントのメール通知が、正常に解除されました。',
+    'Unsubscribed' => '通知が解除されました。',
+);
+
+1;
